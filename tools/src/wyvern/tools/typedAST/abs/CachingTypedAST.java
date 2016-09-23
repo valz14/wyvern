f5d1ef00b9e911257492196d456a2aba8f88c041
Robbie McKinstry <rem89@pitt.edu>
2016-09-23 13:47:20 -0400

    Delete unused imports
    in the files that are already lint-checked.

--- tools/src/wyvern/tools/typedAST/abs/CachingTypedAST.java
+++ tools/src/wyvern/tools/typedAST/abs/CachingTypedAST.java
@@ -1,37 +1,30 @@
 package wyvern.tools.typedAST.abs;
 
 import java.util.Map;
-import java.util.Optional;
 
 import wyvern.tools.typedAST.interfaces.ExpressionAST;
 import wyvern.tools.typedAST.interfaces.TypedAST;
-import wyvern.tools.types.Environment;
 import wyvern.tools.types.Type;
 
 public abstract class CachingTypedAST extends AbstractExpressionAST implements ExpressionAST {
-	private Type type;
-	protected abstract Type doTypecheck(Environment env, Optional<Type> expected);
-	
-	@Override
-	public final Type typecheck(Environment env, Optional<Type> expected) {
-		type = doTypecheck(env, expected);
-		return type;
-	}
-	
-	@Override
-	public final Type getType() {
-		if (type == null)
-			throw new RuntimeException("called getType() before typechecking");
-		return type;
-	}
+    private Type type;
 
+    @Override
+    public final Type getType() {
+        if (type == null) {
+            throw new RuntimeException("called getType() before typechecking");
+        }
+        return type;
+    }
 
-	protected abstract ExpressionAST doClone(Map<String, TypedAST> nc);
-	@Override
-	public ExpressionAST cloneWithChildren(Map<String, TypedAST> nc) {
-		ExpressionAST res = doClone(nc);
-		if (res instanceof CachingTypedAST)
-			((CachingTypedAST) res).type = type;
-		return res;
-	}
+    protected abstract ExpressionAST doClone(Map<String, TypedAST> nc);
+
+    @Override
+    public ExpressionAST cloneWithChildren(Map<String, TypedAST> nc) {
+        ExpressionAST res = doClone(nc);
+        if (res instanceof CachingTypedAST) {
+            ((CachingTypedAST) res).type = type;
+        }
+        return res;
+    }
 }
